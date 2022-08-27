b5b77525009f6556edc4c7ab81fe14afb933ff0f
Michelle David <rachaelholy@gmail.com>
2022-08-28 00:43:21 +0100

    Generate random number

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import Die from './components/Die'
+import React, {useState, useEffect} from "react";
+import Die from './components/Die';
 
 
 export default function App(){
+  const [dice, setDice] = useState(dieRandomNum)
+
+    function dieRandomNum(){
+        const newArray = []
+        for(let i = 0; i < 10; i++){
+            newArray.push(Math.ceil(Math.random() * 6))
+        }
+        return newArray
+    }
+
+    const mapDice = dice.map(die => <Die value={die} />)
+
     return (
         <>
         <main>
             <div className='main-container'>
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
+                {mapDice}
+              
             </div>
         </main>
         </>
