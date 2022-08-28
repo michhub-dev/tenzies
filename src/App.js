d81c21529fe1f4df75ae58e14a3e0f1b0812dbf3
Michelle David <rachaelholy@gmail.com>
2022-08-28 18:17:26 +0100

    Used nanoid, saved through array object

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, {useState, useEffect} from "react";
 import Die from './components/Die';
+import {nanoid} from "nanoid"
 
 
 export default function App(){
-  const [dice, setDice] = useState(dieRandomNum)
+  const [dice, setDice] = useState(dieRandomNum())
 
     function dieRandomNum(){
         const newArray = []
         for(let i = 0; i < 10; i++){
-            newArray.push(Math.ceil(Math.random() * 6))
+            newArray.push({
+                value: Math.ceil(Math.random() * 6), 
+                isHeld: false,
+                id: nanoid()
+        })
         }
         return newArray
     }
 
-    const mapDice = dice.map(die => <Die value={die} />)
+    const mapDice = dice.map(die => <Die key={die.id} value={die.value} />)
 
+    function rollDice(){
+        return setDice(dieRandomNum())
+    }
     return (
         <>
         <main>
             <div className='main-container'>
                 {mapDice}
-              
+              <button className="roll-btn" onClick={rollDice}>Roll Dice</button>
             </div>
         </main>
         </>
